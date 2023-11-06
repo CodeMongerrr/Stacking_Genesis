20d06cea79ffcf4d63c29d96d295cd72e71e5d32
bitofcoding <92008241+bitofcoding@users.noreply.github.com>
2023-11-06 16:29:19 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/lib/index.js"),
+      name: "core-package",
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["react", "react-router-dom"],
+    },
+  },
+});
